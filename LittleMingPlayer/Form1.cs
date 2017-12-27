b3a2768cc479ae27908e350bdca6e6725b837fa1
arvin <li3741@qq.com>
2017-12-28 00:43:54 +0800

    Add project files.

---
+++ LittleMingPlayer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LittleMingPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            player.PlayList.AddRange(System.IO.Directory.GetFiles(@"C:\Users\li374\Music\", "*.mp3", System.IO.SearchOption.AllDirectories));
+            player.SetPlayTime(1);
+            QuartzHelper.InitJobs(player,"0,4,0,5");
+        }
+        PlayerHelper player = new PlayerHelper();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                player.PlayList.Add(open.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.Play(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            player.Stop();
+            player.Dispose();
+            base.OnFormClosing(e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player.Rewind();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                player.PlayList.AddRange(System.IO.Directory.GetFiles(folder.SelectedPath, "*.mp3", System.IO.SearchOption.AllDirectories));
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            player.PlayNext();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            player.PlayPreview();
+        }
+    }
+}
---
+++ LittleMingPlayer/PlayerHelper.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.Collections.Generic;
+using System.Timers;
+
+public class PlayerHelper : IDisposable
+{
+    private Timer playTime;
+    private WaveOutEvent outputDevice;
+    private AudioFileReader audioFileReader;
+    public List<string> PlayList;
+    public int PlayListIndex { get; private set; }
+    public PlayStyle PlayStyle { get; set; }
+    public PlayerHelper()
+    {
+        PlayList = new List<string>();
+        PlayListIndex = -1;
+        PlayStyle = PlayStyle.list;
+
+    }
+    public void SetPlayTime(int minute)
+    {
+        playTime = new Timer();
+        playTime.Interval = 1000 * minute * 60;
+        playTime.Elapsed += PlayTime_Elapsed;
+        playTime.Start();
+    }
+
+    private void PlayTime_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        outputDevice?.Pause();
+    }
+
+    public void ClearList()
+    {
+        PlayList.Clear();
+        PlayListIndex = -1;
+    }
+
+    public int Play(int index = -1)
+    {
+        if (index < 0 || PlayList.Count == 0)
+        {
+            return -1;
+        }
+        if (PlayList.Count <= index)
+        {
+            index = 0;
+        }
+        audioFileReader = new AudioFileReader(PlayList[index]);
+        if (outputDevice != null)
+        {
+            outputDevice.Pause();
+        }
+        outputDevice = new WaveOutEvent();
+        outputDevice.PlaybackStopped += OutputDevice_PlaybackStopped;
+        outputDevice.Init(audioFileReader);
+        outputDevice.Play();
+        PlayListIndex = index;
+        return PlayListIndex;
+    }
+
+
+    public void OutputDevice_PlaybackStopped(object sender, StoppedEventArgs e)
+    {
+        if (PlayStyle != PlayStyle.single)
+        {
+            Play(GetNextStepIndex());
+        }
+    }
+
+    private int GetNextStepIndex()
+    {
+        int nextIndex = -1;
+        switch (PlayStyle)
+        {
+            case PlayStyle.single:
+                nextIndex = PlayListIndex;
+                break;
+            default: break;
+            case PlayStyle.list:
+                nextIndex = PlayList.Count < PlayListIndex ? 0 : PlayListIndex + 1;
+                break;
+            case PlayStyle.random:
+                nextIndex = new Random().Next(0, PlayList.Count - 1);
+                break;
+        }
+        return nextIndex;
+    }
+
+    /// <summary>
+    /// 重新播放
+    /// </summary>
+    public void Rewind()
+    {
+        if (outputDevice?.PlaybackState != PlaybackState.Playing)
+            return;
+        if (audioFileReader != null)
+        {
+            audioFileReader.Position = 0;
+        }
+    }
+
+    public void Stop()
+    {
+        if (outputDevice?.PlaybackState != PlaybackState.Playing)
+            return;
+        outputDevice?.Stop();
+    }
+    public void SetVolume(int vol = 20)
+    {
+        if (audioFileReader != null)
+        {
+            audioFileReader.Volume = vol / 100f;
+        }
+    }
+
+    public void PlayNext()
+    {
+        if (outputDevice?.PlaybackState != PlaybackState.Playing)
+            return;
+        Play(GetNextStepIndex());
+    }
+
+    public void PlayPreview()
+    {
+        if (outputDevice?.PlaybackState != PlaybackState.Playing)
+            return;
+        switch (PlayStyle)
+        {
+            case PlayStyle.single:
+            case PlayStyle.list:
+                Play(PlayListIndex > 0 ? PlayListIndex - 1 : PlayList.Count - 1);
+                break;
+            case PlayStyle.random:
+                Play(GetNextStepIndex());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void AddToList(string fileName)
+    {
+        PlayList.Add(fileName);
+    }
+
+    public void AddRangeToList(IEnumerable<string> files)
+    {
+        PlayList.AddRange(files);
+    }
+
+    public void Remove(string fileName)
+    {
+        if (PlayList.Contains(fileName))
+        {
+            PlayList.Remove(fileName);
+        }
+    }
+
+    public void RemoveIndex(int index)
+    {
+        if (PlayList.Count > index && index > 0)
+        {
+            PlayList.RemoveAt(index);
+        }
+    }
+
+    public string GetPlayingFile()
+    {
+        return PlayList[PlayListIndex];
+    }
+
+    public void Dispose()
+    {
+        if (outputDevice != null)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+        }
+        if (audioFileReader != null)
+        {
+            audioFileReader.Dispose();
+            audioFileReader = null;
+        }
+    }
+}
+
+
+public enum PlayStyle
+{
+    single = 0,
+    list = 1,
+    random = 2,
+}
